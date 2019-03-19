ebb8efd47f9a213934f2361517335f631b4c12d0
Kyran Magpantay <magpantay.kyran@gmail.com>
2019-03-19 15:14:52 -0700

    Major changes to acuity_bot
    - Used "from ... import" instead of "import" to save on overhead
    - Actually used the "main" convention
    - Using platform.uname() instead of sys.os to prevent Bash on Ubuntu on Windows from being ID'd as Linux (which causes problems/errors with sound output)
    - Restructured print outputs and what I'm looking for from the parsed JSON file to make the program more usable in multiple locations/calendars rather than the single location that it was originally created for (which doesn't exist anymore, sadly)
    - Minor bug fixes
    
    - To-do: look at the GET request output for the different calendars to see what I need to access in the parsed JSON file for different locations (non-off-campus locations)

--- acuity_bot/acuity_bot.py
+++ acuity_bot/acuity_bot.py
@@ -1,37 +1,27 @@
-import sys
-import time
-import requests
-import json
-
-# The following enables the beep sound for linux
-if sys.platform == "linux" or sys.platform == "linux2":
-	import os
-	# Beep drivers are removed by default in some linux distros (eg. Ubuntu)
-	# To temporarily enable for current session, (until reboot)
-	os.system("sudo modprobe pcspkr")
-	os.system("xset b 100") # Perhaps not necissary, mine was set to 40.
-	os.system("pactl upload-sample /usr/share/sounds/ubuntu/notifications/Mallet.ogg bell.ogg") # default bing sound. You may choose another (see to-do)
-	# now to make beep, simply run:
-	# os.system("printf \a")
+from platform import uname as os_id	#platform.uname()
+from time import sleep 			#time.sleep()
+from requests import get as web_get 	#requests.get()
+from json import loads as json_parser 	#json.loads()
+from sys import stdout as sysout 	#sys.stdout()
 
 def print0(text, times, isBeeping = 0):
 	print text,
 	for i in range(times):
 		if isBeeping == 0:
-			time.sleep(1)
-		sys.stdout.write('') # keeps text - done inline
-		sys.stdout.flush()
+			sleep(1)
+		sysout.write('') # keeps text - done inline
+		sysout.flush()
 		if isBeeping == 1:
-			if sys.platform == "linux" or sys.platform == "linux2": # beep on linux
-				os.system("printf \a")
-			else: 													# beep on windows
-				sys.stdout.write("\a")
-				sys.stdout.flush()
-			time.sleep(1.25)
+			if os_id()[0].find("Linux") != -1 and os_id()[2].find("Microsoft") == -1: # beep on linux
+				system("printf \a")
+			else: 											    # beep on windows
+				sysout.write("\a")
+				sysout.flush()
+			sleep(1.25)
 	print " - done."
 
 def printJSON(parsed_json_result):
-	count = 1
+	count = 1 # used for the list numbering
 	print "============================================================BEGIN=============================================================\n"
 	for each_result in parsed_json_result:
 		print "--------------"
@@ -40,104 +30,133 @@ def printJSON(parsed_json_result):
 		print "Phone: {0}".format(each_result['phone'])
 		print "Email: {0}".format(each_result['email'])
 		print "Appointment Time: {0}-{1}".format(each_result['time'], each_result['endTime'])
-		print "Type of Service: {0}".format(each_result['forms'][0]['name'])
-
-		if each_result['forms'][0]['name'] == "Dorm Run Request":
-			print "Dorm Location: {0} {1}".format(each_result['forms'][0]['values'][2]['value'], each_result['forms'][0]['values'][3]['value'])
-			print "What's Broken in the Dorm: {0}".format(each_result['forms'][0]['values'][0]['value'])
-			print "Problem Elaboration by User: {0}".format(each_result['forms'][0]['values'][1]['value'])
-
+		print "Type of Service: {0}".format(each_result['type'])
 		print "Work Order/Reference Number: {0}".format(each_result['forms'][1]['values'][0]['value'])
+
+		if each_result['forms'][0]['name'].find("Campus Location") != -1:
+			print ""
+			print "{0}".format(each_result['forms'][0]['name'])
+			print "Location: {0} {1}".format(each_result['forms'][0]['values'][0]['value'], each_result['forms'][0]['values'][1]['value']) #location + rm/ste value
+
+		print ""
+
+		#print "Appointment Notes: {0}".format(each_result['forms'][0]['values'][1]['value']) #//to-do
+		#if each_result['forms'][0]['name'] == "Dorm Run Request":
+		#	print "Dorm Location: {0} {1}".format(each_result['forms'][0]['values'][2]['value'], each_result['forms'][0]['values'][3]['value'])
+		#	print "What's Broken in the Dorm: {0}".format(each_result['forms'][0]['values'][0]['value'])
+		#	print "Problem Elaboration by User: {0}".format(each_result['forms'][0]['values'][1]['value'])
+
+		# the above is commented out because the location this was made for doesn't exist anymore
+
 		print "--------------"
 		count+=1
 	print "\n=============================================================END==============================================================\n"
 
-first_time_running = 1			#acts slightly differently on first run-through
-numTimes_already_set = 0
+def printy(parsed_json_result):
+	print "============================================================BEGIN=============================================================\n"
+	for each_result in parsed_json_result:
+		print each_result
+	print "\n=============================================================END==============================================================\n"
 
-# the following gathers information from a user-supplied INI file
-config_file = open("acuitybotconfig.ini")
-config_file_contents = config_file.read()
-config_file.close()
+def main():
+	first_time_running = 1			#acts slightly differently on first run-through
+	numTimes_already_set = 0
 
-# first remove the preceding part and first single quotation mark
-userID = config_file_contents.split("userID='")[1]		# the 0th index gets the stuff preceding the split
-calendarID = config_file_contents.split("calendarID='")[1]	# meanwhile, the 1st index gets stuff after split
-keyAPI = config_file_contents.split("keyAPI='")[1]
+	print "OS Detected as {0} {1}".format(os_id()[0], os_id()[2])
 
-if config_file_contents.find("numberOfTimes='") != -1:
-	numberOfTimes = config_file_contents.split("numberOfTimes='")[1]
-	numTimes_already_set = 1
+	# The following enables the beep sound for linux
+	if os_id()[0].find("Linux") != -1 and os_id()[2].find("Microsoft") == -1: # is running non-Windows OS/non-Windows Linux
+		from os import system
+		# Beep drivers are removed by default in some linux distros (eg. Ubuntu, but not Bash on Ubuntu on Windows)
+		# To temporarily enable for current session, (until reboot)
+		system("sudo modprobe pcspkr")
+		system("xset b 100") # Perhaps not necessary, mine was set to 40.
+		system("pactl upload-sample /usr/share/sounds/ubuntu/notifications/Mallet.ogg bell.ogg") # default bing sound. You may choose another (see to-do)
+		# now to make beep, simply run:
+		# system("printf \a")
 
-del config_file_contents	# no need for it anymore, just taking up space
+	# the following gathers information from a user-supplied INI file
+	config_file = open("acuitybotconfig.ini")
+	config_file_contents = config_file.read()
+	config_file.close()
 
-# then remove the quotation mark on the other end
-userID = userID.split("'")[0]
-calendarID = calendarID.split("'")[0]
-keyAPI = keyAPI.split("'")[0]
+	# first remove the preceding part and first single quotation mark
+	userID = config_file_contents.split("userID='")[1]		# the 0th index gets the stuff preceding the split
+	calendarID = config_file_contents.split("calendarID='")[1]	# meanwhile, the 1st index gets stuff after split
+	keyAPI = config_file_contents.split("keyAPI='")[1]
 
-if numTimes_already_set:
-	numberOfTimes = numberOfTimes.split("'")[0]
-	numberOfTimes = int(numberOfTimes)
+	if config_file_contents.find("numberOfTimes='") != -1:
+		numberOfTimes = config_file_contents.split("numberOfTimes='")[1]
+		numTimes_already_set = 1
 
-# now that we're done with that, onto the rest of the program
+	del config_file_contents	# no need for it anymore, just taking up space
 
-params = (
-    ('minDate', 'TODAY'),
-    ('maxDate', 'TODAY'),
-    ('calendarID', calendarID),
-)
+	# then remove the quotation mark on the other end
+	userID = userID.split("'")[0]
+	calendarID = calendarID.split("'")[0]
+	keyAPI = keyAPI.split("'")[0]
+	
+	if numTimes_already_set:
+		numberOfTimes = numberOfTimes.split("'")[0]
+		numberOfTimes = int(numberOfTimes)
 
-if numTimes_already_set != 1:
-	numberOfTimes = raw_input("Times to play beep (default: 5): ")
-	if numberOfTimes == "":
-		numberOfTimes = "5"			#default is 5
-	numberOfTimes = int(numberOfTimes)
+	# now that we're done with that, onto the rest of the program
 
-old_len = 0 # used to keep track of amount of appointments
+	params = ( ('minDate', 'TODAY'), ('maxDate', 'TODAY'), ('calendarID', calendarID), )
 
-while 1:
-	something_new = 0
-	appt_cancelled = 0
+	if numTimes_already_set != 1:
+		numberOfTimes = raw_input("Times to play beep (default: 5): ")
+		if numberOfTimes == "":
+			numberOfTimes = "5"			#default is 5
+		numberOfTimes = int(numberOfTimes)
 
-	response = requests.get('https://acuityscheduling.com/api/v1/appointments', params=params, auth=(userID, keyAPI))
-	parsed_json_result = json.loads(response.text)	#must be response.text because reponse just prints the code (i.e., 200 OK or 403 FORBIDDEN, etc.), # type dictionary
+	old_len = 0 # used to keep track of amount of appointments
 
-	if len(parsed_json_result) > old_len and not first_time_running: # something_new doesn't trip on first iterance
-		something_new = 1
+	while 1:
+		something_new = 0
+		appt_cancelled = 0
 
-	if old_len > len(parsed_json_result) and not first_time_running:
-		appt_cancelled = 1
+		response = web_get('https://acuityscheduling.com/api/v1/appointments', params=params, auth=(userID, keyAPI))
+		parsed_json_result = json_parser(response.text)	#must be response.text because reponse just prints the code (i.e., 200 OK or 403 FORBIDDEN, etc.), # type dictionary
 
-	if not first_time_running:
-		print0("Running", 2)
+		if len(parsed_json_result) > old_len and not first_time_running: # something_new doesn't trip on first iterance
+			something_new = 1
 
-		if something_new:
-			time.sleep(0.5)
-			print "\nThere's a new appointment."
-			time.sleep(0.5)
-			printJSON(parsed_json_result)
-			print0("Playing beep {0} times".format(numberOfTimes), numberOfTimes, 1)
-			time.sleep(0.3)
-			print "\n=================\n"
-		elif appt_cancelled:	# need to account for whether an appointment has been cancelled (but it only plays half the amount of specified beeps)
-			time.sleep(0.5)
-			print "\nSomeone cancelled their appointment."
-			time.sleep(0.5)
-			printJSON(parsed_json_result)
-			print0("Playing beep {0} times".format(numberOfTimes/2), numberOfTimes/2, 1)
-			time.sleep(0.3)
-			print "\n================\n"
+		if old_len > len(parsed_json_result) and not first_time_running:
+			appt_cancelled = 1
+
+		if not first_time_running:
+			print0("Running", 2)
+
+			if something_new:
+				sleep(0.5)
+				print "\nThere's a new appointment."
+				sleep(0.5)
+				printJSON(parsed_json_result)
+				print0("Playing beep {0} times".format(numberOfTimes), numberOfTimes, 1)
+				sleep(0.3)
+				print "\n=================\n"
+			elif appt_cancelled:	# need to account for whether an appointment has been cancelled (but it only plays half the amount of specified beeps)
+				sleep(0.5)
+				print "\nSomeone cancelled their appointment."
+				sleep(0.5)
+				printJSON(parsed_json_result)
+				print0("Playing beep {0} times".format(numberOfTimes/2), numberOfTimes/2, 1)
+				sleep(0.3)
+				print "\n================\n"
+			else:
+				sleep(0.5)
+				print "\nNothing new."
+				sleep(0.3)
+				print "\n=================\n"
+
 		else:
-			time.sleep(0.5)
-			print "\nNothing new."
-			time.sleep(0.3)
-			print "\n=================\n"
+			printJSON(parsed_json_result)
+			first_time_running = 0
+			print "First time running, will sleep for 5 seconds and check again.\n"
 
-	else:
-		printJSON(parsed_json_result)
-		first_time_running = 0
-		print "First time running, will sleep for 5 seconds and check again.\n"
+		print0("Sleeping for 5 seconds", 5) # this plus all the other delays means a refresh happens about every 5-10 seconds
+		old_len = len(parsed_json_result)
 
-	print0("Sleeping for 5 seconds", 5) # this plus all the other delays means a refresh happens about every 5-10 seconds
-	old_len = len(parsed_json_result)
+if __name__ == "__main__":
+	main()
